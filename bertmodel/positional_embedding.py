bcf9dd29cfaa904bf2dc123e28f5cd6ebdebc1a5
QuangTran1011 <quangtran101104@gmail.com>
2024-11-19 19:23:10 +0700

    model

---
+++ bertmodel/positional_embedding.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+
+def positional_embedding(positions, d_model):
+    if d_model%2 == 1 : d_model+=1 # d_model must be even 
+    position = np.arange(positions)[:, np.newaxis]
+    k = np.arange(d_model)[np.newaxis, :]
+    i = k//2
+    angle_rates = 1/np.power(10000,2*i/np.float32(d_model))
+    angle_rads = position*angle_rates
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = angle_rads[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+class PositionalEmbedding(tf.keras.layers.Layer):
+    def __init__(self, vocab_size, d_model):
+        super().__init__()
+        self.d_model = d_model
+        self.embedding = tf.keras.layers.Embedding(vocab_size, d_model, mask_zero=True)
+        self.positional_embedding = positional_embedding(positions=256, d_model=d_model)
+
+    def compute_mask(self, *args, **kwargs):
+        return self.embedding.compute_mask(*args, **kwargs)
+    
+    def call(self, x):
+        length = tf.shape(x)[1]
+        x = self.embedding(x)
+        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        x = x + self.positional_embedding[:,:length,:]
+        return x
